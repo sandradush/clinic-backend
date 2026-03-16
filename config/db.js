46b47d9821a3ebb2a410bcaea11b5547df4fdcfe
sandradush <dshmmnsandra@gmail.com>
2026-03-16 11:51:31 +0200

    error solved

--- config/db.js
+++ config/db.js
@@ -2,12 +2,19 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected database error:', err.message);
 });
 
 module.exports = pool;
